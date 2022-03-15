37bfcc666f84fb95b095df335966067f4ed702e9
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-15 15:37:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sw-resistance-social-network
---
+++ src/main/java/br/com/tulio/swresistancesocialnetwork/SwResistanceSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.tulio.swresistancesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwResistanceSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwResistanceSocialNetworkApplication.class, args);
+    }
+
+}
