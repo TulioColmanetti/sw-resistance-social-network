19f149012fa9e23f8d3c49a90bcf688aa573c011
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-16 20:14:32 -0300

    Created location dto for update functionality.

---
+++ src/main/java/br/com/tulio/swresistancesocialnetwork/dto/LocationDTO.java
@@ -0,0 +1,28 @@
+package br.com.tulio.swresistancesocialnetwork.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationDTO {
+
+    @DecimalMin(value = "0.0")
+    @DecimalMax(value = "1000.00")
+    @NotNull
+    private Double latitude;
+
+    @DecimalMin(value = "0.0")
+    @DecimalMax(value = "1000.00")
+    @NotNull
+    private Double longitude;
+
+    @NotNull
+    @NotBlank(message = "Base name is mandatory")
+    private String baseName;
+
+}
