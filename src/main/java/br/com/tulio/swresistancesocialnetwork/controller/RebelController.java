a8fec95bfc40934c67f7ee1c5e57fdb4de0c06c2
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-16 20:17:23 -0300

    Created three new functions on rebel controller for:
    - list all;
    - find by id;
    - update location.

--- src/main/java/br/com/tulio/swresistancesocialnetwork/controller/RebelController.java
+++ src/main/java/br/com/tulio/swresistancesocialnetwork/controller/RebelController.java
@@ -1,6 +1,8 @@
 package br.com.tulio.swresistancesocialnetwork.controller;
 
+import br.com.tulio.swresistancesocialnetwork.dto.LocationDTO;
 import br.com.tulio.swresistancesocialnetwork.dto.RebelDTO;
+import br.com.tulio.swresistancesocialnetwork.exceptions.RebelNotFoundException;
 import br.com.tulio.swresistancesocialnetwork.services.RebelService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rebels")
@@ -19,5 +22,20 @@ public class RebelController {
     @ResponseStatus(HttpStatus.CREATED)
     public RebelDTO createRebel(@RequestBody @Valid RebelDTO rebelDTO) {
         return rebelService.createRebel(rebelDTO);
+    }
+
+    @GetMapping
+    public List<RebelDTO> listAll(){
+        return rebelService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public RebelDTO findById(@PathVariable Long id) throws RebelNotFoundException {
+        return rebelService.findById(id);
+    }
+
+    @PatchMapping("/{id}/updateLocation")
+    public RebelDTO updateLocation(@PathVariable Long id, @RequestBody @Valid LocationDTO locationDTO) throws RebelNotFoundException {
+        return rebelService.updateLocation(id, locationDTO);
     }
 }
