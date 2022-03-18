a8d43143670488c68ad76c2df383e60c136c92e4
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-03-18 01:10:27 -0300

    Added validation to Report dto class

--- src/main/java/br/com/tulio/swresistancesocialnetwork/dto/ReportDTO.java
+++ src/main/java/br/com/tulio/swresistancesocialnetwork/dto/ReportDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
@@ -12,8 +16,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReportDTO {
+
+    @DecimalMin(value = "0.0")
+    @NotNull
     private Double traitorsPercentage;
+
+    @DecimalMin(value = "0.0")
+    @NotNull
     private Double rebelsPercentage;
+
+    @Valid
+    @NotNull
     private List<Double> averageItemQuantityPerRebel;
+
+    @DecimalMin(value = "0.0")
+    @NotNull
     private Long itemPointsLostDueToTraitors;
 }
